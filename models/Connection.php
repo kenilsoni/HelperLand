711c5e2a43d3238474dcab215438e82bfb3e3f18
kenilsoni <68726586+kenilsoni@users.noreply.github.com>
2022-02-03 19:21:46 +0530

    Add files via upload

--- models/Connection.php
+++ models/Connection.php
@@ -19,13 +19,13 @@ class Helperland
     }
 
 
-    public function insert_contact($name, $mobile, $email, $message, $subject)
+    public function insert_contact($name, $mobile, $email, $message, $subject, $date, $filename)
     {
 
-        $sql = "INSERT INTO contactus (Name,PhoneNumber,Email,Message,Subject)
-        VALUES (?,?,?,?,?)";
-        $stmt= $this->conn->prepare($sql);
-        $stmt->execute([$name, $mobile, $email, $message, $subject]);
+        $sql = "INSERT INTO contactus (Name,PhoneNumber,Email,Message,Subject,CreatedOn,UploadFileName)
+        VALUES (?,?,?,?,?,?,?)";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute([$name, $mobile, $email, $message, $subject, $date, $filename]);
 
         echo "<script>alert('Mail send Successfully');
                 window.location.href = '?function=contactpage'; </script>";
@@ -35,47 +35,140 @@ class Helperland
         // for userprovider set usertypeid to 1
         // for serviceprovider set usertypeid to 2
         $stmt = $this->conn->prepare("SELECT * FROM user WHERE Email=?");
-        $stmt->execute([$email]); 
+        $stmt->execute([$email]);
         $user = $stmt->fetch();
-        if($user){
+        if ($user) {
             echo "<script>alert('Email is already exist'); window.location.href = '?function=Homepage';</script>";
-        }
-        else{
-              $sql = "INSERT INTO user (FirstName,LastName,Email,Password,Mobile,UserTypeId)
+        } else {
+            $sql = "INSERT INTO user (FirstName,LastName,Email,Password,Mobile,UserTypeId)
         VALUES (?,?,?,?,?,?)";
-        $stmt= $this->conn->prepare($sql);
-        $stmt->execute([$fname, $lname,$email,$password, $mobile,$UserTypeId]);
+            $stmt = $this->conn->prepare($sql);
+            $stmt->execute([$fname, $lname, $email, $password, $mobile, $UserTypeId]);
 
-        echo "<script>alert('Account created successfully');
+            echo "<script>alert('Account created successfully');
                 window.location.href = '?function=Homepage'; </script>";
         }
-    
-      
     }
-    public function check_login($email,$password){
+    public function check_login($email, $password, $remember)
+    {
         $query = "select * from user where Email=? and Password=?";
-        $stmt= $this->conn->prepare($query);
-        $stmt->execute([$email,$password]); 
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute([$email, $password]);
         $user = $stmt->fetchAll(PDO::FETCH_ASSOC);
-       
-        if($user){
+
+        if ($user) {
+            if (!empty($remember)) {
+
+                setcookie("member_login", $email, time() + (86400 * 3));
+                setcookie("member_password", $password, time() + (86400 * 3));
+            }
             session_start();
             foreach ($user as $users) {
-                    $user_id=$users['UserTypeId'] ;
-                    $_SESSION['user_name']=$users['FirstName'] ;
+                $user_type = $users['UserTypeId'];
+                $_SESSION['user_id'] = $users['UserId'];
+                $_SESSION['user_name'] = $users['FirstName'];
             }
-            if($user_id == 1){
-                  header("location:?function=service_historypage");
+            if ($user_type == 1) {
+                header("location:?function=service_historypage");
             }
-            if($user_id == 2){
+            if ($user_type == 2) {
                 header("location:?function=upcoming_servicepage");
             }
-        }
-        else{
+        } else {
             echo "<script>alert('Please check email or password');
             window.location.href = '?function=Homepage';</script>";
-        
         }
+    }
 
+    public function check_pincode($pincode)
+    {
+        $query = "select id from zipcode where ZipcodeValue=?";
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute([$pincode]);
+        $user = $stmt->fetch();
+        if ($user) {
+            setcookie("pincode", $pincode, time() + 3600);
+            header("location:?controller=Helperland&function=bookservice_page&flag=second");
+        } else {
+            echo "<script>alert('Sorry!! Service is not available here');
+            window.location.href='?controller=Helperland&function=bookservice_page'</script>";
+        }
+    }
+    public function getaddress($userid)
+    {
+        $query = "select AddressLine1,AddressLine2,City,State,PostalCode,Mobile	 from useraddress where UserId=?";
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute([$userid]);
+        $user = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        if ($user) {
+            // foreach ($user as $users) { echo $users['AddressLine1'];}
+
+            $_SESSION['addressdata'] = $user;
+            header("location:?controller=Helperland&function=bookservice_page&flag=third");
+        } else {
+        }
+    }
+    public function insertschedule($data, $extraservice)
+    {
+        $sql = "INSERT INTO servicerequest (UserId,ServiceId,ServiceStartDate,ZipCode,ServiceHourlyRate,ServiceHours,ExtraHours,SubTotal,Discount,TotalCost,Comments,HasPets,CreatedDate)
+        VALUES (:UserId,:ServiceId,:ServiceStartDate,:ZipCode,:ServiceHourlyRate,:ServiceHours,:ExtraHours,:SubTotal,:Discount,:TotalCost,:Comments,:HasPets,:CreatedDate)";
+        $stmt = $this->conn->prepare($sql);
+        $success = $stmt->execute($data);
+        $id = $this->conn->lastInsertId();
+        $sql2 = "INSERT INTO servicerequestextra (ServiceExtraId,ServiceRequestId) VALUES (?,?)";
+        $stmt2 = $this->conn->prepare($sql2);
+        $success2 = $stmt2->execute([$extraservice, $id]);
+        if ($success2 && $success) {
+            echo "<script>alert('your booking is successfully');
+                window.location.href='?controller=Helperland&function=HomePage';
+                </script>";
+        } else {
+            echo "something went wrong";
+        }
+    }
+    public function check_email($emailid)
+    {
+        $stmt = $this->conn->prepare("SELECT UserId FROM user WHERE Email=?");
+        $stmt->execute([$emailid]);
+        $user = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        if ($user) {
+            foreach ($user as $users) {
+                $id = $users['UserId'];
+            }
+            require "vendor/autoload.php";
+            $email = new \SendGrid\Mail\Mail();
+            $email->setFrom("comicbykenil@gmail.com", "HelperLand");
+            $email->setSubject("Forgot Password");
+            $email->addTo($emailid);
+            $email->addContent(
+                "text/html",
+                "<h1> Please click on the given link to forgot your password </h1><br><a href='http://localhost/helperland/1st_submission/?controller=Helperland&function=forgotpassword_page&Parameter=$id'>Please Click Here to Forgot Password</a>"
+
+            );
+            $sendgrid = new \SendGrid("");
+            if ($sendgrid->send($email)) {
+                echo "<script>alert('Mail sent Successfully!!')
+                window.location.href='?controller=Helperland&function=HomePage'</script>";
+            } else {
+                echo "<script>alert('Something went wrong')</script>";
+            }
+        } else {
+            echo "<script>alert('Email is not exist in our record');
+            window.location.href='?controller=Helperland&function=HomePage';</script>";
+        }
+    }
+    public function update_password($password, $id)
+    {
+
+        $sql = "UPDATE user SET Password=? where UserId=?";
+        $stmt = $this->conn->prepare($sql);
+        $user = $stmt->execute([$password, $id]);
+        if ($user) {
+            echo "<script>alert('Password is Update Successfully');
+            window.location.href='?controller=Helperland'</script>";
+        } else {
+            echo "something went wrong";
+        }
     }
 }
