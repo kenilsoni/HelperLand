45ab8d3b52bc57b647e1bd0d68c5f233c8c38884
kenilsoni <68726586+kenilsoni@users.noreply.github.com>
2022-02-07 20:25:14 +0530

    Add files via upload

--- models/Connection.php
+++ models/Connection.php
@@ -19,34 +19,66 @@ class Helperland
     }
 
 
-    public function insert_contact($name, $mobile, $email, $message, $subject, $date, $filename)
+    public function insert_contact($data)
     {
-
+        session_start();
         $sql = "INSERT INTO contactus (Name,PhoneNumber,Email,Message,Subject,CreatedOn,UploadFileName)
-        VALUES (?,?,?,?,?,?,?)";
+        VALUES (:Name,:PhoneNumber,:Email,:Message,:Subject,:CreatedOn,:UploadFileName)";
         $stmt = $this->conn->prepare($sql);
-        $stmt->execute([$name, $mobile, $email, $message, $subject, $date, $filename]);
-
-        echo "<script>alert('Mail send Successfully');
-                window.location.href = '?function=contactpage'; </script>";
+        $execute = $stmt->execute($data);
+        if ($execute) {
+            $_SESSION['contact'] = 1;
+            echo "<script>
+            window.location.href = '?function=contactpage'; </script>";
+        } else {
+            $_SESSION['contact'] = 2;
+            echo "<script>
+            window.location.href = '?function=contactpage'; </script>";
+        }
     }
-    public function insert_user($fname, $lname, $mobile, $email, $password, $UserTypeId)
+    public function insert_user($data, $email)
     {
         // for userprovider set usertypeid to 1
         // for serviceprovider set usertypeid to 2
-        $stmt = $this->conn->prepare("SELECT * FROM user WHERE Email=?");
-        $stmt->execute([$email]);
+
+        session_start();
+        $usertype = $data['UserTypeId'];
+        $stmt = $this->conn->prepare("SELECT * FROM user WHERE Email=:Email");
+        $stmt->execute($email);
         $user = $stmt->fetch();
         if ($user) {
-            echo "<script>alert('Email is already exist'); window.location.href = '?function=Homepage';</script>";
+            $_SESSION['emailexist'] = 1;
+            if ($usertype == 1) {
+                echo "<script>window.location.href = '?function=createaccountpage';</script>";
+            } else {
+                echo "<script>
+                window.location.href = '?function=become_providerpage'; </script>";
+            }
         } else {
-            $sql = "INSERT INTO user (FirstName,LastName,Email,Password,Mobile,UserTypeId)
-        VALUES (?,?,?,?,?,?)";
+            $sql = "INSERT INTO user (FirstName,LastName,Email,Password,Mobile,UserTypeId,CreatedDate)
+        VALUES (:FirstName,:LastName,:Email,:Password,:Mobile,:UserTypeId,:CreatedDate)";
             $stmt = $this->conn->prepare($sql);
-            $stmt->execute([$fname, $lname, $email, $password, $mobile, $UserTypeId]);
+            $execute = $stmt->execute($data);
+            if ($execute) {
 
-            echo "<script>alert('Account created successfully');
+                $_SESSION['registration'] = 1;
+                if ($usertype == 1) {
+                    echo "<script>
                 window.location.href = '?function=Homepage'; </script>";
+                } else {
+                    echo "<script>
+                    window.location.href = '?function=become_providerpage'; </script>";
+                }
+            } else {
+                $_SESSION['registration'] = 2;
+                if ($usertype == 1) {
+                    echo "<script>
+                window.location.href = '?function=Homepage'; </script>";
+                } else {
+                    echo "<script>
+                    window.location.href = '?function=become_providerpage'; </script>";
+                }
+            }
         }
     }
     public function check_login($email, $password, $remember)
@@ -55,14 +87,14 @@ class Helperland
         $stmt = $this->conn->prepare($query);
         $stmt->execute([$email, $password]);
         $user = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
+        session_start();
         if ($user) {
             if (!empty($remember)) {
 
                 setcookie("member_login", $email, time() + (86400 * 3));
                 setcookie("member_password", $password, time() + (86400 * 3));
             }
-            session_start();
+
             foreach ($user as $users) {
                 $user_type = $users['UserTypeId'];
                 $_SESSION['user_id'] = $users['UserId'];
@@ -75,7 +107,8 @@ class Helperland
                 header("location:?function=upcoming_servicepage");
             }
         } else {
-            echo "<script>alert('Please check email or password');
+            $_SESSION['checkemail'] = 1;
+            echo "<script>
             window.location.href = '?function=Homepage';</script>";
         }
     }
@@ -90,13 +123,15 @@ class Helperland
             setcookie("pincode", $pincode, time() + 3600);
             header("location:?controller=Helperland&function=bookservice_page&flag=second");
         } else {
-            echo "<script>alert('Sorry!! Service is not available here');
-            window.location.href='?controller=Helperland&function=bookservice_page'</script>";
+            session_start();
+            $_SESSION['pincode'] = 1;
+            echo "<script>
+            window.location.href='?controller=Helperland&function=bookservice_page';</script>";
         }
     }
     public function getaddress($userid)
     {
-        $query = "select AddressLine1,AddressLine2,City,State,PostalCode,Mobile	 from useraddress where UserId=?";
+        $query = "select AddressLine1,AddressLine2,City,State,PostalCode,Mobile	 from useraddress where UserId=? limit 2";
         $stmt = $this->conn->prepare($query);
         $stmt->execute([$userid]);
         $user = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -106,24 +141,42 @@ class Helperland
             $_SESSION['addressdata'] = $user;
             header("location:?controller=Helperland&function=bookservice_page&flag=third");
         } else {
+            echo "<script>
+            window.location.href='?controller=Helperland&function=bookservice_page';</script>";
         }
     }
-    public function insertschedule($data, $extraservice)
+    public function insertschedule($service_data, $extraservice, $address)
     {
         $sql = "INSERT INTO servicerequest (UserId,ServiceId,ServiceStartDate,ZipCode,ServiceHourlyRate,ServiceHours,ExtraHours,SubTotal,Discount,TotalCost,Comments,HasPets,CreatedDate)
         VALUES (:UserId,:ServiceId,:ServiceStartDate,:ZipCode,:ServiceHourlyRate,:ServiceHours,:ExtraHours,:SubTotal,:Discount,:TotalCost,:Comments,:HasPets,:CreatedDate)";
         $stmt = $this->conn->prepare($sql);
-        $success = $stmt->execute($data);
+        $success1 = $stmt->execute($service_data);
         $id = $this->conn->lastInsertId();
+
         $sql2 = "INSERT INTO servicerequestextra (ServiceExtraId,ServiceRequestId) VALUES (?,?)";
         $stmt2 = $this->conn->prepare($sql2);
         $success2 = $stmt2->execute([$extraservice, $id]);
-        if ($success2 && $success) {
-            echo "<script>alert('your booking is successfully');
+
+        $sql3 = "INSERT INTO servicerequestaddress (  `AddressLine1`, `AddressLine2`, `City`, `PostalCode`, `Mobile`,`ServiceRequestId`) VALUES (:AddressLine1, :AddressLine2, :City,  :PostalCode, :Mobile,$id)";
+        $stmt3 = $this->conn->prepare($sql3);
+        $success3 = $stmt3->execute($address);
+
+
+        $userid = $_SESSION['user_id'];
+        $sql4 = "INSERT INTO useraddress ( `AddressLine1`, `AddressLine2`, `City`,  `PostalCode`,`Mobile`,`UserId`) VALUES (:AddressLine1, :AddressLine2, :City,  :PostalCode, :Mobile,$userid)";
+        $stmt4 = $this->conn->prepare($sql4);
+        $success4 = $stmt4->execute($address);
+
+        if ($success2 && $success1 && $success3 && $success4) {
+            $_SESSION['booking'] = 1;
+            echo "<script>
                 window.location.href='?controller=Helperland&function=HomePage';
                 </script>";
         } else {
-            echo "something went wrong";
+            $_SESSION['booking'] = 2;
+            echo "<script>
+            window.location.href='?controller=Helperland&function=HomePage';
+            </script>";
         }
     }
     public function check_email($emailid)
@@ -131,7 +184,7 @@ class Helperland
         $stmt = $this->conn->prepare("SELECT UserId FROM user WHERE Email=?");
         $stmt->execute([$emailid]);
         $user = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
+        session_start();
         if ($user) {
             foreach ($user as $users) {
                 $id = $users['UserId'];
@@ -148,27 +201,34 @@ class Helperland
             );
             $sendgrid = new \SendGrid("");
             if ($sendgrid->send($email)) {
-                echo "<script>alert('Mail sent Successfully!!')
+                $_SESSION['sendmail'] = 1;
+                echo "<script>
                 window.location.href='?controller=Helperland&function=HomePage'</script>";
             } else {
-                echo "<script>alert('Something went wrong')</script>";
+                $_SESSION['sendmail'] = 2;
+                echo "<script>
+                window.location.href='?controller=Helperland&function=HomePage'</script>";
             }
         } else {
-            echo "<script>alert('Email is not exist in our record');
+            $_SESSION['sendmail'] = 3;
+            echo "<script>
             window.location.href='?controller=Helperland&function=HomePage';</script>";
         }
     }
     public function update_password($password, $id)
     {
-
+        session_start();
         $sql = "UPDATE user SET Password=? where UserId=?";
         $stmt = $this->conn->prepare($sql);
         $user = $stmt->execute([$password, $id]);
         if ($user) {
-            echo "<script>alert('Password is Update Successfully');
+            $_SESSION['fpassword'] = 1;
+            echo "<script>
             window.location.href='?controller=Helperland'</script>";
         } else {
-            echo "something went wrong";
+            $_SESSION['fpassword'] = 2;
+            echo "<script>
+            window.location.href='?controller=Helperland'</script>";
         }
     }
 }
